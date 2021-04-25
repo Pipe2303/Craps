3f812a858343fd03722286cc1ddd64acae5367f0
Felipe Roldan Restrepo <piperoldanrestrepo@hotmail.com>
2021-04-24 23:48:51 -0500

    Avance del juego condiciones, cuadro animado, link para creditos

--- js/main.js
+++ js/main.js
@@ -1,54 +1,69 @@
     var tiro_1;
-    var tiro_2;
-    var posiciones = [0,-160,-320,-481,-642,-803];
-    var dado1,dado2,boton_tirar;
-    var suma;
+var tiro_2;
+var posiciones = [0,-160,-320,-481,-642,-803];
+var dado1,dado2,boton_tirar;
+var suma;
+var turno = 1;
 
+window.onload = init;
 
-
-
-    window.onload = init;
-
-
-
-    function init(){
+function init(){
     boton_tirar = document.getElementById("boton_tirar");
     boton_tirar.addEventListener("click",jugar);
 
-
-
     dado1 = document.getElementById("dado1");
     dado2 = document.getElementById("dado2");
-    }
+  cerrar.addEventListener("click",cerrarVentana);
+}
 
 
-
-    function tirardado(){
+function tirardado(){
     return Math.floor(Math.random() * 6) + 1 ;
-    }
+}
 
+function actualizarDado(ref,cara){
+  ref.style.backgroundPosition = posiciones[cara-1]+"px";
+}
 
+function jugar(){
+  tiro_1 = tirardado();
+  tiro_2 = tirardado();
+  actualizarDado(dado1,tiro_1);
+  actualizarDado(dado2,tiro_2);
 
-    function actualizarDado(ref,cara){
-    ref.style.backgroundPosition = posiciones[cara-1]+"px";
-    }
+ 
+  suma = tiro_1 + tiro_2;
 
+  turno = turno + 1;
 
+  if(suma==7 || suma==11)
+  {
+    turno = 1;
+    console.log("Se reinicio el turno");
+    mostrarMensaje("Gano la partida");
+  }
+else(suma==12 || suma==2 || suma==3)
+  {
+    turno = 1;
+    console.log("Se reinicio el turno");
+    mostrarMensaje("Perdio la partida");
+  }
+ if(suma==4 || suma==5 || suma==6 || suma==8 || suma==9 || suma==10)
+   {
+      turno = 1;
+    mostrarMensaje("Ganaste un punto");
+   }
+}
 
-    function jugar(){
-    tiro_1 = tirardado();//Retorna un numero entre 1 y 6
-    tiro_2 = tirardado();
-    actualizarDado(dado1,tiro_1);
-    actualizarDado(dado2,tiro_2);
+function mostrarMensaje(mensaje){
+  mensaje_texto.innerHTML = mensaje;
+  abrirVentana();
+}
 
-    suma = tiro_1 + tiro_2 
+function abrirVentana(){  
+  ventana.className = "ligthbox animate__animated animate__fadeIn";
+}
 
-    if(suma == 7 || suma == 11)
-    {
-        alert("ganaste")
-    }
-    else
-    {
-        alert("sigue intentando")
-    }
-    }
+function cerrarVentana(){
+  ventana.className = "ligthbox hidden";
+}
