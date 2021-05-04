e0b8168973fdfd276f3b662b13c43149615e0029
Felipe Roldan Restrepo <piperoldanrestrepo@hotmail.com>
2021-05-03 23:32:11 -0500

    Por fin, Codigo dados js final

--- js/main.js
+++ js/main.js
@@ -1,69 +1,98 @@
-    var tiro_1;
+var tiro_1;
 var tiro_2;
 var posiciones = [0,-160,-320,-481,-642,-803];
 var dado1,dado2,boton_tirar;
 var suma;
-var turno = 1;
+var turno=1;
+var punto=0;
+
 
 window.onload = init;
 
 function init(){
-    boton_tirar = document.getElementById("boton_tirar");
-    boton_tirar.addEventListener("click",jugar);
+boton_tirar =document.getElementById("boton_tirar");
+boton_tirar.addEventListener("click",jugar);
 
-    dado1 = document.getElementById("dado1");
-    dado2 = document.getElementById("dado2");
-  cerrar.addEventListener("click",cerrarVentana);
+dado1 = document.getElementById("dado1");
+dado2 = document.getElementById("dado2");
+cerrar.addEventListener("click",cerrarVentana);
 }
 
-
 function tirardado(){
-    return Math.floor(Math.random() * 6) + 1 ;
+return Math.floor(Math.random() * 6) + 1 ;
 }
 
 function actualizarDado(ref,cara){
-  ref.style.backgroundPosition = posiciones[cara-1]+"px";
+ref.style.backgroundPosition = posiciones[cara-1]+"px";
 }
 
 function jugar(){
-  tiro_1 = tirardado();
-  tiro_2 = tirardado();
-  actualizarDado(dado1,tiro_1);
-  actualizarDado(dado2,tiro_2);
+tiro_1 = tirardado();
+tiro_2 = tirardado();
+actualizarDado(dado1,tiro_1);
+actualizarDado(dado2,tiro_2);
 
- 
-  suma = tiro_1 + tiro_2;
 
-  turno = turno + 1;
+suma = tiro_1 + tiro_2;
 
-  if(suma==7 || suma==11)
+if((suma==7 || suma==11) && turno==1 )
+{
+  mostrarMensaje("😎Gano la partida😎");
+  console.log("Se reinicio el turno");
+  turno=1;
+}
+else
+{
+  if((suma==2 || suma==3 || suma==12) && turno==1)
   {
-    turno = 1;
+    mostrarMensaje("🤕 Perdio la partida 🤕");
     console.log("Se reinicio el turno");
-    mostrarMensaje("Gano la partida");
+    turno=1; 
   }
-if(suma==12 || suma==2 || suma==3)
+  else
   {
-    turno = 1;
-    console.log("Se reinicio el turno");
-    mostrarMensaje("Perdio la partida");
-  }
- if(suma==4 || suma==5 || suma==6 || suma==8 || suma==9 || suma==10)
-   {
+    if(turno==1)
+    {
+      punto = suma;
+      mostrarMensaje("El Punto es " + punto);
+      console.log("Punto es ", punto);
+    }
+
+    if(suma == punto && turno>=2)
+    {
+      mostrarMensaje("😎 Gano la partida 😎");
+      console.log("Se reinicio el turno");
       turno = 1;
-    mostrarMensaje("Ganaste un punto");
-   }
+      punto = 0;
+    }
+    else
+    {
+      if(suma == 7)
+      {
+        mostrarMensaje("🤕 Perdio la partida 🤕");
+        console.log("Se reinicio el turno");
+        turno = 1;
+        punto = 0;
+      }
+      else
+      {
+        turno = turno + 1;
+      }
+    }
+  }
+}
 }
 
+
 function mostrarMensaje(mensaje){
-  mensaje_texto.innerHTML = mensaje;
-  abrirVentana();
+mensaje_texto.innerHTML = mensaje;
+abrirVentana();
 }
 
 function abrirVentana(){  
-  ventana.className = "ligthbox animate__animated animate__fadeIn";
+ventana.className = "ligthbox animate__animated  animate__fadeIn";
 }
 
 function cerrarVentana(){
-  ventana.className = "ligthbox hidden";
+ventana.className = "ligthbox hidden";
 }
